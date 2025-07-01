2bdfc2db994a2acbe619c4593f36c828f323eed2
Rich Nazareta <richnazareta@gmail.com>
2025-07-01 11:57:49 +0800

    second commit

--- sql-script/sql-scripts.sql
+++ sql-script/sql-scripts.sql
@@ -9,11 +9,20 @@ CREATE TABLE IF NOT EXISTS product (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS customer(
+	id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    contact_number VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS sale (
 	sale_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     date DATETIME NOT NULL DEFAULT current_timestamp,
     total_amount DECIMAL(10, 2) NOT NULL,
-    customer_name VARCHAR(255)
+    customer_id BIGINT NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS sale_item(
@@ -24,4 +33,5 @@ CREATE TABLE IF NOT EXISTS sale_item(
     price FLOAT NOT NULL,
     FOREIGN KEY (sale_id) REFERENCES sale(sale_id) ON DELETE CASCADE,
     FOREIGN KEY (product_id) REFERENCES product(product_id) ON DELETE CASCADE
+    
 );
