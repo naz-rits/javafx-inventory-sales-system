306e3db106f3c176b7e0574db110cf65428bece6
Rich Nazareta <richnazareta@gmail.com>
2025-07-05 14:49:39 +0800

    fixed logout button

--- src/main/java/com/javafxsalesmanagementsystem/ui/MainUI.java
+++ src/main/java/com/javafxsalesmanagementsystem/ui/MainUI.java
@@ -1,8 +1,11 @@
 package com.javafxsalesmanagementsystem.ui;
 
 import com.javafxsalesmanagementsystem.entity.Customer;
+import com.javafxsalesmanagementsystem.entity.Session;
 import com.sun.javafx.font.freetype.HBGlyphLayout;
+import com.sun.tools.javac.Main;
 import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
@@ -10,17 +13,18 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -94,8 +98,24 @@ public class MainUI {
 
         Button login = new Button("Login");
 
+        Label customerName = new Label();
         if (hasLogin) {
-            login.setText(customer.get().getUsername());
+            customerName = new Label(customer.get().getUsername());
+            customerName.setFont(Font.font("Segoe UI", FontWeight.BOLD, 30));
+            customerName.setTextFill(Color.BLACK);
+            customerName.setTranslateY(30);
+            customerName.setTranslateX(25);
+            customerName.setBackground(Background.fill(Color.DARKGRAY));
+            customerName.setPadding( new Insets(5));
+            customerName.setBorder(Border.stroke(Color.DARKGRAY));
+            customerName.setStyle("-fx-background-color: #d9d9d9;" +
+                    "-fx-scale-z: 1.5;" +
+                    "-fx-text-fill: white;" +
+                    "-fx-border-width: 50;" +
+                    "-fx-border-radius: 50;" +
+                    "-fx-background-radius: 999;" +
+                    "-fx-min-width: 50;" +
+                    "-fx-min-height: 40;");
         }
 
         login.setStyle("-fx-background-color: green;" +
@@ -122,8 +142,37 @@ public class MainUI {
             loginAndRegisterUI.loginStage().show();
         });
 
-        HBox hBox = new HBox(650, hasLogin ? cart : cartDisabled, login);
-        VBox root = new VBox(20, productContainer, hasLogin ? new VBox() : productUI.addButton(primaryStage, onRefresh));
+        Button logoutButton = new Button("Logout");
+
+        logoutButton.setStyle("-fx-background-color: red;" +
+                "-fx-scale-z: 1.5;" +
+                "-fx-text-fill: white;" +
+                "-fx-border-width: 50;" +
+                "-fx-border-radius: 50;"
+        );
+        logoutButton.setFont(Font.font("Segoe UI", FontWeight.BOLD, 20));
+        logoutButton.setCursor(Cursor.HAND);
+        logoutButton.setBorder(Border.stroke(Color.BLACK));
+        logoutButton.setPrefWidth(150);
+        logoutButton.setTranslateX(390);
+        logoutButton.setOnAction(e -> {
+            List<Window> windows = new ArrayList<>(Window.getWindows());
+            for (Window window : windows) {
+                if (window instanceof Stage stage1 && stage1.isShowing()) {
+                    stage1.close();
+                }
+            }
+            MainUI mainUI = applicationContext.getBean(MainUI.class);
+
+            Session.currentUser = Optional.empty();
+            mainUI.initialStage(Optional.empty()).show();
+
+        });
+
+
+
+        HBox hBox = new HBox(650, hasLogin ? cart : cartDisabled, hasLogin ? new VBox(50, customerName, logoutButton): login);
+        VBox root = new VBox(20, productContainer, hasLogin ? logoutButton : productUI.addButton(primaryStage, onRefresh));
         root.setAlignment(Pos.CENTER);
         VBox roots = new VBox(10, hBox, root);
         String image = Objects.requireNonNull(getClass().getResource("/images/coffee_background.jpg")).toExternalForm();
